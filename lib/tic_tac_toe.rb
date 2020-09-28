ec24f65f70bb466719e9982627a182f52389f548
Alexander Bowers <7319abowers@gmail.com>
2020-09-28 17:57:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -130,7 +130,8 @@ def winner(board)
   if combo_array = won?(board)
     #no iterations here
     index = combo_array[0]
-    puts "Congratulations player #{board[index]}"
     return board[index]
   end
 end
+
+def congratulate(winner(board))
